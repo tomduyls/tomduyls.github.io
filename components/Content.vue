94d4f49df2aae575f8edf27b8cd6938edf8bdda9
tomduyls <duybuiduc1@gmail.com>
2024-09-18 15:01:22 +0700

    WIP

--- components/Content.vue
+++ components/Content.vue
@@ -1,27 +1,39 @@
 <template>
   <div class="wrapper">
-    <div class="main h-screen" id="1">
-      <div
-        class="glitch"
-        :class="isBlur && 'blur'"
-        :data-text="text"
-        @click="
-          isBlur = true;
-          text = 'Blured';
-        "
-      >
-        {{ text }}
+    <div class="marquee">
+      <span> You spin me right round, baby. Like a record, baby. </span>
+    </div>
+    <div class="main h-screen d-flex flex-column" id="1">
+      <div style="overflow: hidden">
+        <div class="text text-in" style="font-size: 128px">HEHE</div>
+      </div>
+      <div style="overflow: hidden" class="hehe-in">
+        <div class="text mt-16 text-in" style="font-size: 64px">13 12 2001</div>
       </div>
     </div>
-    <div class="main h-screen" id="2">
-      <!-- <div>Testing</div> -->
-      <img
+    <div class="main h-screen position-relative" id="2">
+      <!-- <img
         id="2"
         width="100%"
         height="100%"
         style="object-fit: cover"
         src="https://www.stellar-blade.com/resources/front/images/post_ms7/01.jpg"
-      />
+      /> -->
+      <div class="position-absolute bottom-0 left-0">
+        <div
+          class="w-100 text-center font-weight-bold text-uppercase bg-red"
+          style="font-size: 24px"
+        >
+          Live Reaction
+        </div>
+        <img
+          id="2"
+          width="200px"
+          height="200px"
+          src="/public/very_angry.png"
+          style="object-fit: contain"
+        />
+      </div>
     </div>
     <div class="main py-6" id="3">Created by the one and only - Dym</div>
   </div>
@@ -32,7 +44,6 @@ export default {
   data() {
     return {
       isBlur: false,
-      text: "Glitch Text",
       isActive: 1,
       touchStart: undefined,
       touchEnd: undefined,
@@ -54,6 +65,9 @@ export default {
       this.touchEnd = event.changedTouches[0].clientY;
       this.handleScroll(event);
     });
+    // const observer = new IntersectionObserver((entries, observer) => {}, {
+    //   threshold: 1,
+    // });
   },
   methods: {
     handleScroll(e) {
@@ -123,10 +137,48 @@ export default {
 
 <style lang="scss" scoped>
 .wrapper {
-  background-image: url("/public/adam_pizurny.gif");
+  // background-image: url("/public/adam_pizurny.gif");
   background-size: cover;
   background-color: black;
+  position: relative;
 }
+
+@keyframes marquee {
+  0% {
+    transform: translateX(50%);
+  }
+  100% {
+    transform: translateX(-100%);
+  }
+}
+
+.marquee {
+  position: fixed;
+  padding: 3px;
+  width: 100vh;
+  left: -44vh;
+  top: 48.3vh;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  -ms-transform: rotate(-90deg);
+  transform: rotate(-90deg);
+  overflow: hidden;
+  span {
+    will-change: transform;
+    -webkit-transform: translateX(50%);
+    -moz-transform: translateX(50%);
+    -o-transform: translateX(50%);
+    -ms-transform: translateX(50%);
+    transform: translateX(50%);
+    white-space: nowrap;
+    width: 100vh;
+    text-align: end;
+    animation: marquee 13s linear infinite;
+  }
+}
+
 .main {
   color: white;
   display: flex;
@@ -134,203 +186,27 @@ export default {
   justify-content: center;
 }
 
-.glitch {
-  position: relative;
-  font-size: 3rem;
-  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
-    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
-  font-weight: bold;
-  color: #ffffff;
-  letter-spacing: 3px;
-  z-index: 1;
-}
-
-.blur {
-  animation: blur-it 0.6s both;
-}
-
-.glitch:before {
-  content: attr(data-text);
-  position: absolute;
-  top: 0;
-  left: -2px;
-  width: 100%;
-  color: 50px;
-  background-color: #000000;
-  overflow: hidden;
-  clip: rect(0, 900px, 0, 0);
-  animation: noise-before 5s infinite linear alternate-reverse;
-}
-
-.glitch:after {
-  content: attr(data-text);
-  position: absolute;
-  top: 0;
-  left: 2px;
-  width: 100%;
-  color: 50px;
-  background-color: #000000;
-  overflow: hidden;
-  clip: rect(0, 900px, 0, 0);
-  animation: noise-after 10s infinite linear alternate-reverse;
-}
-
-@keyframes noise-before {
+@keyframes text-in {
   0% {
-    clip: rect(61px, 9999px, 52px, 0);
-  }
-  5% {
-    clip: rect(33px, 9999px, 144px, 0);
-  }
-  10% {
-    clip: rect(121px, 9999px, 115px, 0);
-  }
-  15% {
-    clip: rect(144px, 9999px, 162px, 0);
-  }
-  20% {
-    clip: rect(62px, 9999px, 180px, 0);
-  }
-  25% {
-    clip: rect(34px, 9999px, 42px, 0);
-  }
-  30% {
-    clip: rect(147px, 9999px, 179px, 0);
-  }
-  35% {
-    clip: rect(99px, 9999px, 63px, 0);
-  }
-  40% {
-    clip: rect(188px, 9999px, 122px, 0);
-  }
-  45% {
-    clip: rect(154px, 9999px, 14px, 0);
-  }
-  50% {
-    clip: rect(63px, 9999px, 37px, 0);
-  }
-  55% {
-    clip: rect(161px, 9999px, 147px, 0);
-  }
-  60% {
-    clip: rect(109px, 9999px, 175px, 0);
-  }
-  65% {
-    clip: rect(157px, 9999px, 88px, 0);
-  }
-  70% {
-    clip: rect(173px, 9999px, 131px, 0);
-  }
-  75% {
-    clip: rect(62px, 9999px, 70px, 0);
-  }
-  80% {
-    clip: rect(24px, 9999px, 153px, 0);
-  }
-  85% {
-    clip: rect(138px, 9999px, 40px, 0);
-  }
-  90% {
-    clip: rect(79px, 9999px, 136px, 0);
-  }
-  95% {
-    clip: rect(25px, 9999px, 34px, 0);
+    transform: translateY(110%);
   }
   100% {
-    clip: rect(173px, 9999px, 166px, 0);
+    transform: translateY(0%);
   }
 }
 
-@keyframes noise-after {
-  0% {
-    clip: rect(26px, 9999px, 33px, 0);
+.hehe-in {
+  transition: ease-in-out 0.8s;
+  &:hover {
+    .text-in {
+      transform: translateY(0%);
+    }
   }
-  5% {
-    clip: rect(140px, 9999px, 198px, 0);
-  }
-  10% {
-    clip: rect(184px, 9999px, 89px, 0);
-  }
-  15% {
-    clip: rect(121px, 9999px, 6px, 0);
-  }
-  20% {
-    clip: rect(181px, 9999px, 99px, 0);
-  }
-  25% {
-    clip: rect(154px, 9999px, 133px, 0);
-  }
-  30% {
-    clip: rect(134px, 9999px, 169px, 0);
-  }
-  35% {
-    clip: rect(26px, 9999px, 187px, 0);
-  }
-  40% {
-    clip: rect(147px, 9999px, 137px, 0);
-  }
-  45% {
-    clip: rect(31px, 9999px, 52px, 0);
-  }
-  50% {
-    clip: rect(191px, 9999px, 109px, 0);
-  }
-  55% {
-    clip: rect(74px, 9999px, 54px, 0);
-  }
-  60% {
-    clip: rect(145px, 9999px, 75px, 0);
-  }
-  65% {
-    clip: rect(153px, 9999px, 198px, 0);
-  }
-  70% {
-    clip: rect(99px, 9999px, 136px, 0);
-  }
-  75% {
-    clip: rect(118px, 9999px, 192px, 0);
-  }
-  80% {
-    clip: rect(1px, 9999px, 83px, 0);
-  }
-  85% {
-    clip: rect(145px, 9999px, 98px, 0);
-  }
-  90% {
-    clip: rect(121px, 9999px, 154px, 0);
-  }
-  95% {
-    clip: rect(156px, 9999px, 44px, 0);
-  }
-  100% {
-    clip: rect(67px, 9999px, 122px, 0);
-  }
-}
-
-@keyframes blur-it {
-  0% {
-    filter: blur(0px);
-    transform: translate(0);
-  }
-  20% {
-    filter: blur(2px);
-    transform: translate(-2px, 0px);
-  }
-  40% {
-    filter: blur(4px);
-    transform: translate(-5px, 0px);
-  }
-  60% {
-    filter: blur(6px);
-    transform: translate(8px, 0px);
-  }
-  80% {
-    filter: blur(8px);
-    transform: translate(10px, 0px);
-  }
-  to {
-    filter: blur(0px);
-    transform: translate(0);
+  .text-in {
+    transition: all 1s;
+    padding: 0.2em 0.1em;
+    // animation: text-in 1s ease-out;
+    transform: translateY(110%);
   }
 }
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,4 +12,14 @@ export default defineNuxtConfig({
       /* vuetify options */
     },
   },
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Alumni+Sans:ital,wght@0,100..900;1,100..900&family=Quicksand:wght@300..700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap",
+        },
+      ],
+    },
+  },
 });
